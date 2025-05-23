70f9043a46897649a59a2596f019a8d7dd0a23fc
karthik.kunnathadath <karthik.kunnathadath@maqta.ae>
2025-05-23 10:56:01 +0400

    new learning

---
+++ PythonSamples/ran.py
@@ -0,0 +1,12 @@
+import random
+arabiclist=['خ','س','ض','غ','ق','ي','ـم']
+
+a1=random.choice(arabiclist)
+a2=random.choice(arabiclist)
+a3=random.choice(arabiclist)
+a4=random.choice(arabiclist)
+a5=random.choice(arabiclist)
+a6=random.choice(arabiclist)
+a7=random.choice(arabiclist)
+
+print(a1+a2+a3+a4+a5+a6+a7)
