4471cf33373c640d29c5948e01fdfdcb37118151
karthik.kunnathadath <karthik.kunnathadath@maqta.ae>
2025-07-01 11:50:32 +0400

    test

--- pytestPlaywright/testcases/conftest.py
+++ pytestPlaywright/testcases/conftest.py
@@ -1,6 +1,7 @@
 import pytest
 from playwright.sync_api import sync_playwright
 from utilities import configReader
+import allure
 
 @pytest.fixture(scope="function")
 def browser():
@@ -10,15 +11,27 @@ def browser():
         browser.close()
 
 
+@pytest.fixture(autouse=True)
 def setup_function(page):
-    page.goto(configReader.readconfig("basicInfo","testsiteurl"))
+    print("\n...Go to base url...")
+    page.goto(configReader.readConfig("basicInfo","testsiteurl"))
     
 
 @pytest.fixture(scope="function")
 def page(browser):
+    print("\n....conftest page...")
     context=browser.new_context()
+    global page
     page=context.new_page()
     ##### page=browser.new_page()
     page.set_viewport_size({"width":1920,"height":1020})
     yield page
     page.close()
+    
+    
+@pytest.fixture()
+def log_on_failure(request):
+    yield
+    item=request.node
+    if item.rep_call.failed:
+        allure.attach(page.screenshot(path="screenshot/fullpage.png"),name="failureScreenshot",attachment_type=allure.attachment_type.PNG)
--- pytestPlaywright/testcases/test_pw.py
+++ pytestPlaywright/testcases/test_pw.py
@@ -1,9 +1,24 @@
 from playwright.sync_api import Page
+import allure
+import pytest
 
+## pytest ./testcases/test_pw.py -s --html=report.html
+
+
+@allure.feature("Sample test")
+@allure.severity(allure.severity_level.MINOR)
+@pytest.mark.usefixtures("log_on_failure")
 def test_pw_1(page):
-    page.goto("https://demo.automationtesting.in/Register.html")
-    title=page.title()
-    print(title)
-    page.wait_for_timeout(2000)
+    with allure.step("fill details"):
+        page.goto("https://demo.automationtesting.in/Register.html")
+        title=page.title()
+        print(title)
+        page.locator('//input[@placeholder="First Name"]').fill("karthik")
+        allure.attach(page.screenshot(path="screenshot/fullpage.png"),name="dologin",attachment_type=allure.attachment_type.PNG)
+        # page.wait_for_timeout(4000)
+        assert 1==2
+        
+    # allure.attach(page.screenshot(path="screenshot/fullpage.png"),name="dologin",attachment_type=allure.attachment_type.PNG)
     
+        
     
